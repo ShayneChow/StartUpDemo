03ad1c5fe1744dde312a4faedce2d6ad812e4d0d
Shayne Chow <shayne@authbuy.org>
2015-03-09 10:36:54 +0800

    Initial Commit

---
+++ StartUpDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StartUpDemo
+//
+//  Created by ChowShayne on 15/3/9.
+//  Copyright (c) 2015年 ShayneChow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
